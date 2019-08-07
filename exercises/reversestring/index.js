01ecb9f5886943be83126831f1d94afa5a89f7f1
Kenny <kawhyte@gmail.com>
2019-08-07 10:36:06 -0700

    Init

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+
+console.log ("kenny");
+
+function reverse(str) {
+const arr = str.split('');
+
+
+ arr.forEach(str => {
+     arr === str;
+ });
+
+ return arr.reverse();
+
+
+}
+
+module.exports = reverse;
