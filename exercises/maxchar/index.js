81bf9d736db8044a27887d3ccc54dbc4b343e066
Kenny <kawhyte@gmail.com>
2019-08-09 11:19:18 -0700

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let i of str) {
+    if (!chars[i]) {
+      chars[i] = 1;
+      //console.log(chars[i]);
+    } else {
+      chars[i]++;
+    }
+
+    for (const key in chars) {
+      if (chars[key] > max) {
+        max = chars[key];
+        maxChar = key;
+      }
+    }
+  }
+  console.log(maxChar);
+  return maxChar;
+}
 
 module.exports = maxChar;
