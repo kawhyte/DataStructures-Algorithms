f2add8c55e457ccd3547a4ed9fa049f41346e800
Kenny <kawhyte@gmail.com>
2019-08-14 11:59:59 -0700

    Capitalize V1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,19 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const splitted = str.split(' ');
+    let newArr =[];
+
+   for (const word of splitted) {
+    const firstLetter = word[0].toUpperCase();
+    const sliced = word.slice(1);
+    newArr.push(firstLetter+sliced);
+   }
+   
+   console.log(newArr.join(' '));
+   return newArr.join(' ')
+
+}
 
 module.exports = capitalize;
