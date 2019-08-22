0bfd0d9b0162b2f06fe8eab27e30ed2ff44fab36
Kenny <kawhyte@gmail.com>
2019-08-22 15:03:27 -0700

    insert At

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -132,6 +132,24 @@ class LinkedList {
 
     previousNode.next = previousNode.next.next;
   }
+
+
+  insertAt(data, index){
+    if (!this.head) {
+        this.head = new Node(data);
+        return;
+      }
+
+      if (index === 0) {
+        this.head = new Node(data, this.head);
+        return;
+      }
+
+      const previousNode = this.getAt(index - 1) || this.getLast();
+      const node = new Node(data, previousNode.next);
+      previousNode.next = node;
+
+  }
 }
 
 function testlink() {
