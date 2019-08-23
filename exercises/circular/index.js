74c00fe3280239fd87a520c084ef62fc560e2f2b
Kenny <kawhyte@gmail.com>
2019-08-23 14:07:36 -0700

    Element n spaces from the last node in the list

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -18,14 +18,12 @@ function circular(list) {
     let fast  = list.getFirst()
     
     while (fast.next && fast.next.next) {
-        
         slow = slow.next;
         fast = fast.next.next; 
 
         if (slow === fast) {
             return true;
         }
-
     }
      return false;
 
