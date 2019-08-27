9a838670b2aac7cd19a2882294139362f80ef775
Kenny <kawhyte@gmail.com>
2019-08-27 14:10:15 -0700

    traverseDF

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -20,37 +20,36 @@ class Node {
     this.children.push(node);
   }
 
-  remove(data){
-        this.children = this.children.filter(node =>{
-    return node.data !== data;
-})
-
+  remove(data) {
+    this.children = this.children.filter(node => {
+      return node.data !== data;
+    });
   }
 }
 
 class Tree {
+  constructor() {
+    this.root = null;
+  }
 
-constructor(){
-this.root = null;
+  traverseBF(fn) {
+    const arr = [this.root];
 
-}
- 
+    while (arr.length) {
+      const node = arr.shift();
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
 
-traverseBF(fn) {
-const arr = [this.root];
-
-while (arr.length) {
-    const node = arr.shift();
-
-    arr.push(...node.children);
-    fn(node);
-}
-}
-
-
-
-
-
+  traverseDF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
 }
 
 module.exports = { Tree, Node };
